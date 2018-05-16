e8f561b6230fd24bdff99d4c79522296468fb007
rabib <rabib@shaficonsul;tancy.com>
2018-05-16 12:36:49 +0600

    Updated style and component for the app

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="skills">
     <h1>{{ name }}</h1>
-    <!--  {{ btnState ? 'The button is disabed' : 'The button is active' }}
-    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>-->
     <div class="holder">
         <ul>
-            <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+            <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
         </ul>
-        <div v-bind:class="alertObject"></div>
-        <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
     </div>
-      
-<!--      <p v-if="skills.length >= 1">You have more than one skill</p>
-      <p v-else>You have less than or equal to one skill</p>-->
   </div>
 </template>
 
@@ -27,12 +20,6 @@ export default {
               { "skill": "Vue.JS" },
               { "skill": "Laravel" }
           ],
-          alertObject: {
-            alert: true,
-          },
-          bgColor: 'yellow',
-          bgWidth: '100%',
-          bgHeight: '30px'
       }
   }
 }

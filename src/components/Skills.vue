b223f07d0bba3f672656b3cf526893332e3b9fde
rabib <rabib@shaficonsul;tancy.com>
2018-05-16 12:58:39 +0600

    two way binding and form submit

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,6 +2,10 @@
   <div class="skills">
     <h1>{{ name }}</h1>
     <div class="holder">
+        <form @submit.prevent="addSkill">
+            <input type="text" placeholder="Enter a skill you have" v-model="skill">
+        </form>
+        {{ skill }}
         <ul>
             <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
         </ul>
@@ -14,12 +18,19 @@ export default {
   name: 'Skills',
   data() {
       return {
-          name: 'Rabib',
-          btnState: true,
+          name: 'My Skills',
+          skill: '',
           skills: [
               { "skill": "Vue.JS" },
               { "skill": "Laravel" }
           ],
+      }
+  },
+    // Add this section:
+  methods : {
+      addSkill(){
+          this.skills.push({skill: this.skill});
+          this.skill = '';
       }
   }
 }
